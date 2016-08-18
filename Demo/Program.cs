67c66dcb677e2db506fcff99bed87e7f6839cb8f
Travis O'Sullivan <tosullivan@duosecurity.com>
2016-08-18 15:27:23 -0400

    Created Duo demo using .NET platform

---
+++ Demo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+
+namespace duo_csharp
+{
+    class Program
+    {
+        private static string ikey;
+        private static string skey;
+        private static string akey;
+        private static string host;
+        private static string port;
+
+        static void Main(string[] args)
+        {
+            ParseConfiguration();
+            WebServer server = new WebServer(SendResponse, String.Format("http://localhost:{0}/", port));
+            server.Run();
+            PrintStartupMessaging();
+            Console.ReadKey();
+            server.Stop();
+        }
+
+        private static void PrintStartupMessaging()
+        {
+            Console.WriteLine("Server has been started.");
+            Console.WriteLine("Visit the root URL with a 'user' argument, e.g.");
+            Console.WriteLine(String.Format("'http://localhost:{0}/?user=myname'.", port));
+            Console.WriteLine("Press any key to quit.");
+        }
+
+        private static void ParseConfiguration()
+        {
+            ikey = ConfigurationManager.AppSettings["ikey"];
+            skey = ConfigurationManager.AppSettings["skey"];
+            akey = ConfigurationManager.AppSettings["akey"];
+            host = ConfigurationManager.AppSettings["host"];
+            port = ConfigurationManager.AppSettings["port"];
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            if (String.Compare(request.HttpMethod, "POST", true) == 0)
+            {
+                return doPost(request);
+            }
+            return doGet(request);
+        }
+
+        private static string doPost(HttpListenerRequest request)
+        {
+            using (System.IO.Stream body = request.InputStream)
+            {
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(body, request.ContentEncoding))
+                {
+                    String bodyStream = reader.ReadToEnd();
+                    var form = bodyStream.Split('=');
+                    var sig_response_val = System.Net.WebUtility.UrlDecode(form[1]);
+                    String responseUser = Duo.Web.VerifyResponse(ikey, skey, akey, sig_response_val);
+                    if (String.IsNullOrEmpty(responseUser))
+                    {
+                        return "Did not authenticate with Duo.";
+                    }
+                    else
+                    {
+                        return String.Format("Authenticated with Duo as {0}.", responseUser);
+                    }
+                }
+            }
+        }
+
+        private static string doGet(HttpListenerRequest request)
+        {
+            String response = String.Empty;
+
+            try
+            {
+                response = System.IO.File.ReadAllText(System.IO.Path.GetFileName(request.RawUrl));
+            }
+            catch (Exception e)
+            {
+                String userName = request.QueryString.Get("user");
+                if (String.IsNullOrEmpty(userName))
+                    return String.Format("You must include a user to authenticate with Duo");
+
+                var sig_request = Duo.Web.SignRequest(ikey, skey, akey, userName);
+                response = String.Format(@"<html>
+                  <head>
+                    <title>Duo Authentication</title>
+                    <meta name='viewport' content='width=device-width, initial-scale=1'>
+                    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                    <link rel='stylesheet' type='text/css' href='Duo-Frame.css'>
+                  </head>
+                  <body>
+                    <h1>Duo Authentication</h1>
+                    <script src='/Duo-Web-v2.js'></script>
+                    <iframe id='duo_iframe'
+                            title='Two-Factor Authentication'
+                            frameborder='0'
+                            data-host='{0}'
+                            data-sig-request='{1}'>
+                    </iframe>
+                  </body>
+                </html>", host, sig_request);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Demo/WebServer.cs
@@ -0,0 +1,100 @@
+﻿/*
+This simple Web Server was pulled from https://codehosting.net/blog/BlogEngine/post/Simple-C-Web-Server under
+The MIT License (MIT)
+
+Copyright (c) 2013 David's Blog (www.codehosting.net) 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace duo_csharp
+{
+    class WebServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+
+        public WebServer(string[] prefixes, Func<HttpListenerRequest, string> method)
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
+
+            // URI prefixes are required, for example 
+            // "http://localhost:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // A responder method is required
+            if (method == null)
+                throw new ArgumentException("method");
+
+            foreach (string s in prefixes)
+                _listener.Prefixes.Add(s);
+
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
+            : this(prefixes, method)
+        { }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                string rstr = _responderMethod(ctx.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { } // suppress any exceptions
+                            finally
+                            {
+                                // always close the stream
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { } // suppress any exceptions
+            });
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+
+    }
+}
